666b32eb324babdf5d7eaaacef1b9b41afa6c723
Task-1.1 <apzigaziga@gmail.com>
2020-12-27 05:05:00 +0500

    Задание 5.2(2)

--- 05-java-oop-principles/task02/src/com/example/task02/Task02Main.java
+++ 05-java-oop-principles/task02/src/com/example/task02/Task02Main.java
@@ -1,15 +1,14 @@
 package com.example.task02;
-
-public class Task02Main {
-
+public class Task02Main
+{
     private static final Item ITEM1 = new Item("Товар 1", 10);
     private static final Item ITEM2 = new Item("Товар 2", 20);
     private static final Item ITEM3 = new Item("Товар 3", 30);
     private static final Item ITEM4 = new Item("Товар 4", 40);
     private static final Item ITEM5 = new Item("Товар 5", 50);
     private static final Item ITEM6 = new Item("Товар 6", 60);
-
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Bill bill = new Bill();
         bill.add(ITEM1, 10);
         bill.add(ITEM3, 3);
@@ -17,5 +16,12 @@ public class Task02Main {
         System.out.println(bill);
         bill.add(ITEM3, 3);
         System.out.println(bill);
+
+        DiscountBill db = new DiscountBill(50);
+        db.add(ITEM1, 10);
+        db.add(ITEM3, 3);
+        db.add(ITEM6, 1);
+        db.add(ITEM3, 3);
+        System.out.println(db.getPrice());
     }
 }
